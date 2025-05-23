8be7e982d2708d75c0e0b66cd65c232e16264c65
Noe TATOUD <noe.tatoud@gmail.com>
2025-05-23 19:43:55 +0200

    chore: change folder structure

---
+++ src/features/blog/schemas.ts
@@ -0,0 +1,19 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+export const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({
+    base: "./src/features/blog/content",
+    pattern: "**/*.{md,mdx}",
+  }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
