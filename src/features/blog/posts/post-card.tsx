41930b500077c4ed443098dba77d0391622ce112
Noe TATOUD <noe.tatoud@gmail.com>
2025-07-16 19:44:18 +0200

    fix(post-card): mask colors

--- src/features/blog/posts/post-card.tsx
+++ src/features/blog/posts/post-card.tsx
@@ -1,27 +1,28 @@
-import { ArrowRight } from 'lucide-react';
-
+import { cn } from '@/lib/utils';
 import { FormattedDate } from '@/components/formatted-date';
-import { Button } from '@/components/ui/button';
 import type { BlogPost } from '@/features/blog/schemas';
 
 export function PostCard(post: BlogPost & { id: string }) {
   return (
     <a href={`/blog/${post.id}/`} className="group">
-      <article className="rounded-md overflow-hidden group-hover:scale-[101%] bg-sidebar transition-all duration-100">
-        <div className="relative mask-b-from-sidebar mask-b-to-80%">
+      <article className="rounded-md overflow-hidden bg-sidebar transition-all duration-100">
+        <div
+          className={cn(
+            'relative',
+            'mask-b-from-sidebar/40 light:mask-b-from-sidebar t3-chat:mask-b-from-sidebar',
+            'group-hover:mask-b-from-sidebar/60 light:group-hover:mask-b-from-sidebar/90 t3-chat:group-hover:mask-b-from-sidebar/90'
+          )}
+        >
           {!!post.heroImage && (
             <img
               src={post.heroImage.src}
               alt={post.title}
-              className="w-full aspect-video object-cover"
+              className="w-full transition-all aspect-video object-cover group-hover:scale-105"
             />
           )}
-
-          {/* Gradient overlay */}
         </div>
 
-        {/* Actual content – no longer under absolute */}
-        <div className="relative z-10 p-4 space-y-1 -mt-28">
+        <div className="relative p-4 space-y-1 -mt-28">
           <FormattedDate
             date={post.pubDate}
             className="text-xs uppercase opacity-80 font-light"
@@ -30,11 +31,11 @@ export function PostCard(post: BlogPost & { id: string }) {
             {post.title}
           </div>
           <div className="truncate">{post.description}</div>
-          <div className="flex justify-end">
-            <Button size="sm" variant="link">
+          {/* <div className="flex justify-end">
+            <div className={buttonVariants({ size: 'sm', variant: 'link' })}>
               Keep reading <ArrowRight />
-            </Button>
-          </div>
+            </div>
+          </div> */}
         </div>
       </article>
     </a>
