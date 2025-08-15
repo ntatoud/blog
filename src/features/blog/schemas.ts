090b1064ef9075a32480fe95cf65393af70998c0
Noé TATOUD <135032615+ntatoud@users.noreply.github.com>
2025-08-15 18:13:58 +0200

    feat(blog): MCP Article (#13)
    * feat: MCP article
    
    * fix(mcp-article): remove old image
    
    * fix: small style issues
    
    * style: improve bold texts
    
    * feat(blog-post): translations in post meta

--- src/features/blog/schemas.ts
+++ src/features/blog/schemas.ts
@@ -1,10 +1,39 @@
 import { z, type SchemaContext } from 'astro:content';
 
+export type Languages = z.infer<ReturnType<typeof zLanguages>>;
+const zLanguages = () => z.enum(['fr', 'en']);
+export const LANGUAGES_ASSETS: Record<
+  Languages,
+  { flag: string; name: string }
+> = {
+  fr: {
+    flag: '🇫🇷',
+    name: 'French',
+  },
+  en: {
+    flag: '🇬🇧',
+    name: 'English',
+  },
+};
+
 export type BlogPost = z.infer<ReturnType<typeof zBlogPost>>;
 export const zBlogPost = ({ image }: SchemaContext) =>
   z.object({
     title: z.string(),
     description: z.string(),
+    translations: z
+      .array(
+        z
+          .object({
+            lang: zLanguages(),
+            url: z.string().url(),
+          })
+          .transform((val) => ({
+            ...LANGUAGES_ASSETS[val.lang],
+            url: val.url,
+          }))
+      )
+      .optional(),
     tags: z.array(z.string()).optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
