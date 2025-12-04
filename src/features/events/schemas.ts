efb47e682902aed39db1b7b866b5801913e63af6
Noe TATOUD <noe.tatoud@gmail.com>
2025-12-04 16:20:03 +0100

    feat(events): proper listing and structure

--- src/features/events/schemas.ts
+++ src/features/events/schemas.ts
@@ -1,5 +1,14 @@
 import { z, type SchemaContext } from 'astro:content';
 
+export type zEventRole = z.infer<typeof zEventRole>;
+export const zEventRole = z.enum([
+  'attendee',
+  'animator',
+  'speaker',
+  'organizer',
+  'volunteer',
+]);
+
 export type Event = z.infer<ReturnType<typeof zEvent>>;
 export const zEvent = ({ image }: SchemaContext) =>
   z.object({
@@ -10,4 +19,5 @@ export const zEvent = ({ image }: SchemaContext) =>
     link: z.string().url(),
     date: z.coerce.date(),
     heroImage: image().optional(),
+    role: zEventRole,
   });
